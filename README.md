54de2514de716e69cf44b23d2ae6af42a55be46e
Jannick Stranghöner <jannick.stranghoener@uni-bielefeld.de>
2024-11-26 14:16:24 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# constrained_learning
+Handy package for training output constrained neural networks. As of now, this package only contains 
+constrained extreme learning machines (CELM), constrained multi layer neural networks will be added in the future.
+
+# Structure
+The source code is contained in the [src](src) folder.
+The implementation of the (C)ELM can be found in [src/learner.py](src/learner.py).
+
+To install the package, run the following commands inside this repository:
+```
+conda create -n celm python=3.10
+conda activate celm
+pip install -e .
+```
+
+# License
+MIT license - See [LICENSE](LICENSE).   
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='constrained_learning',
+    version='3.1',
+    packages=find_packages(),
+    license=None,
+    description='Handy package for training output constrained neural networks',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "cvxopt>=1.3.1",
+        "dill>=0.3.6",
+        "pynput",
+        "matplotlib>=3.7.1",
+        "numpy",
+        "pyLasaDataset>=0.1.1",
+        "scikit_learn>=1.2.2",
+        "scipy>=1.10.1",
+        "torch>=2.0.1"
+    ],
+    url='https://github.com/ubi-coro/constrained_learning',
+    author='Jannick Stranghöner',
+    author_email='jannick.stranghoener@uni-bielefeld.de'
+)
